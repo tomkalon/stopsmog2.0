5b085ff67ebee40489e9f8c90d2b4cf1e960ba63
Tomasz Kaliński <tomkalon@gmail.com>
2024-04-30 11:25:48 +0200

    added map improvements

--- assets/app.js
+++ assets/app.js
@@ -9,7 +9,10 @@ import './bootstrap.js';
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
+// icons
+import 'fontawesome-4.7'
 
+// bootstrap
 const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
