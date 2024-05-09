0cb027b3de4d268ea458e8c58d3b345370bf57c3
Tomasz Kaliński <tomkalon@gmail.com>
2024-05-09 23:47:48 +0200

    added map image

--- src/UI/Controller/FrontPage/FrontPageController.php
+++ src/UI/Controller/FrontPage/FrontPageController.php
@@ -3,17 +3,21 @@
 namespace App\UI\Controller\FrontPage;
 
 use App\Infrastructure\Query\Sensor\GetSensorListInterface;
+use App\Infrastructure\Query\Settings\GetSettingsViewInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class FrontPageController extends AbstractController
 {
     public function index(
-        GetSensorListInterface $sensorListQuery
+        GetSensorListInterface $sensorListQuery,
+        GetSettingsViewInterface $settingsViewQuery
     ): Response
     {
         $sensorList = $sensorListQuery->execute();
+        $settingsView = $settingsViewQuery->execute();
         return $this->render('Main/FrontPage/index.html.twig', [
+            'settings' => $settingsView,
             'sensorList' => $sensorList
         ]);
     }
