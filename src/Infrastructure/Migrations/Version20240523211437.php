6f7350c82c84c3a654868e7142ea3675851155ed
Tomasz Kaliński <tomkalon@gmail.com>
2024-05-23 23:18:19 +0200

    sensor position

---
+++ src/Infrastructure/Migrations/Version20240523211437.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240523211437 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE sensor ADD position_x INT NOT NULL, ADD position_y INT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE sensor DROP position_x, DROP position_y');
+    }
+}
