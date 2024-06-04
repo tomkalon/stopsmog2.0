67b2492c8ce9c04ae980d553ced446d66f2af93d
Tomasz Kaliński <tomkalon@gmail.com>
2024-06-05 00:34:56 +0200

    react + charts

---
+++ assets/react/components/charts/DayChart.js
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import Container from 'react-bootstrap/Container';
+import Box from '@mui/material/Box';
+
+import { axisClasses } from '@mui/x-charts/ChartsAxis';
+import {BarChart, BarPlot} from '@mui/x-charts/BarChart';
+
+
+export default function DayChart(props) {
+    const xData = []
+    const xLabels = []
+
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+
+    };
+
+    props.data['hydra:member'].forEach((measurement) => {
+        xData.push(measurement.pm10)
+        xLabels.push(new Date(measurement.createdAt))
+    })
+
+    return (
+        <Container>
+            <Box display="flex">
+                <BarChart
+                    width={960}
+                    height={500}
+                    axisHighlight={{
+                        x: 'line'
+                    }}
+                    xAxis={[{
+                        scaleType: 'band',
+                        valueFormatter: (date, context) =>
+                            context.location === 'tick'
+                            ? date.getUTCHours().toString()
+                            : date.toLocaleString(),
+                        data: xLabels,
+                        label: "Godzina",
+                        labelStyle: {
+                            fontSize: 14,
+                            fill: "black",
+                            fontWeight: "bold",
+                            fontFamily: "Poppins"
+                        },
+
+                    }]}
+                    yAxis={[ {
+                        min: 0,
+                        label: "Wartość PM10",
+                        labelStyle: {
+                            fontSize: 14,
+                            fill: "black",
+                            fontWeight: "bold",
+                            fontFamily: "Poppins"
+                        },
+                        colorMap: {
+                            type: 'piecewise',
+                            thresholds: [25, 50, 80, 110, 150],
+                            colors: ['green', 'yellow', 'orange', 'red', '#aa1e1e',  'purple'],
+                        },
+                    }]}
+                    sx={
+                        (theme) => ({
+                            [`.${axisClasses.left} .${axisClasses.label}`]: {
+                                transform: 'translate(-10px, 0)',
+                            },
+                        })
+                    }
+                    series={[{
+                        data: xData,
+                        label: 'PM10',
+                        type: 'bar',
+                        color: '#ffffff'
+                    }]}
+                />
+            </Box>
+        </Container>
+    );
+}
--- assets/react/controllers/Main/Sensor/Sensor.jsx
+++ assets/react/controllers/Main/Sensor/Sensor.jsx
@@ -1,19 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import Api from '@Api';
-import Container from 'react-bootstrap/Container';
-import Box from '@mui/material/Box';
-
-import { axisClasses } from '@mui/x-charts/ChartsAxis';
-import {BarChart, BarPlot} from '@mui/x-charts/BarChart';
+import DayChart from "@ReactComponent/charts/DayChart";
 
 export default function (props) {
 
     const [data, setData] = useState(null);
 
-    useEffect(() => {
-        const today = new Date();
-        today.setHours(0, 0, 0);
+    const today = new Date();
+    today.setHours(0, 0, 0);
 
+    useEffect(() => {
         Api.get(
             'api_measurements_get',
             {
@@ -25,83 +21,7 @@ export default function (props) {
         )
     }, []);
 
-
-    const xData = []
-    const xLabels = []
-
     if (data) {
-        data['hydra:member'].forEach((measurement) => {
-            xData.push(measurement.pm10)
-            xLabels.push(new Date(measurement.createdAt))
-        })
+        return(<DayChart data={data}/>)
     }
-
-    const chartSetting = {
-        yAxis: [
-            {
-                label: 'Wartość PM10',
-            },
-        ],
-
-        width: 900,
-        height: 560,
-        sx: {
-            [`.${axisClasses.left} .${axisClasses.label}`]: {
-                transform: 'translate(-20px, 0)',
-            },
-        },
-    };
-
-    return (
-        <Container>
-            <Box display="flex">
-                <BarChart
-                    width={960}
-                    height={500}
-                    xAxis={[{
-                        scaleType: 'band',
-                        dataKey: 'hour',
-                        valueFormatter: (date) => date.getUTCHours().toString(),
-                        data: xLabels,
-                        label: "Godzina",
-                        labelStyle: {
-                            fontSize: 14,
-                            fill: "black",
-                            fontWeight: "bold",
-                            fontFamily: "Poppins"
-                        },
-
-                    }]}
-                    yAxis={[ {
-                        min: 0,
-                        label: "Wartość PM10",
-                        labelStyle: {
-                            fontSize: 14,
-                            fill: "black",
-                            fontWeight: "bold",
-                            fontFamily: "Poppins"
-                        },
-                        colorMap: {
-                            type: 'piecewise',
-                            thresholds: [25, 50, 80, 110, 150],
-                            colors: ['green', 'yellow', 'orange', 'red', '#aa1e1e',  'purple'],
-                        },
-                    }]}
-                    sx={
-                        (theme) => ({
-                            [`.${axisClasses.left} .${axisClasses.label}`]: {
-                                transform: 'translate(-10px, 0)',
-                            },
-                        })
-                    }
-                    series={[{
-                        data: xData,
-                        label: 'PM10',
-                        type: 'bar',
-                        color: '#ffffff'
-                    }]}
-                />
-            </Box>
-        </Container>
-    );
 }
