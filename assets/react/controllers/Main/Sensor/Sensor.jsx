307f1410204a8313a8927c840e5acc807779b4d5
Tomasz Kaliński <tomkalon@gmail.com>
2024-06-04 17:20:36 +0200

    react + measurements api

--- assets/react/controllers/Main/Sensor/Sensor.jsx
+++ assets/react/controllers/Main/Sensor/Sensor.jsx
@@ -1,8 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import Api from '@Api';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Box from '@mui/material/Box';
+
+import { ChartContainer } from '@mui/x-charts/ChartContainer';
+import { ChartsReferenceLine } from '@mui/x-charts/ChartsReferenceLine';
+import { ChartsXAxis } from '@mui/x-charts/ChartsXAxis';
+import { ChartsYAxis } from '@mui/x-charts/ChartsYAxis';
+import { LinePlot, MarkPlot } from '@mui/x-charts/LineChart';
+import {hydrate} from "react-dom";
 
 export default function (props) {
 
@@ -16,19 +22,56 @@ export default function (props) {
             'api_measurements_get',
             {
                 sensor: props.sensor,
-                'createdAt[after]': today.toISOString(),
+                // 'createdAt[after]': today.toISOString(),
+                'createdAt[after]': "2024-05-23T00:00:00.000Z",
             },
             (data) => setData(data)
         )
     }, []);
 
+
+    const xData = []
+    const xLabels = []
+
+    if (data) {
+        data['hydra:member'].forEach((measurement) => {
+            xData.push(measurement.pm10)
+            xLabels.push(new Date(measurement.createdAt))
+        })
+    }
+
     return (
         <Container>
-            <Row>
-                <Col>
-                    {data ? JSON.stringify(data) : 'Loading...'}
-                </Col>
-            </Row>
+            <Box display="flex">
+                <ChartContainer
+                    width={960}
+                    height={500}
+                    series={[{
+                        data: xData,
+                        label: 'pm10',
+                        type: 'line'
+                    }]}
+                    xAxis={[{
+                        scaleType: 'utc',
+                        valueFormatter: (date) => date.getUTCHours().toString(),
+                        data: xLabels
+                    }]}
+                    yAxis={[ {
+                        min: 0,
+                        colorMap: {
+                            type: 'piecewise',
+                            thresholds: [0, 25, 50, 80, 110, 150],
+                            colors: ['green', 'yellow', 'orange', 'red', 'burgundy',  'purple'],
+                        }
+                    }]}
+                >
+                    <MarkPlot />
+                    <LinePlot />
+                    <ChartsReferenceLine y={50} label="Norma" lineStyle={{ stroke: 'red', strokeDasharray: '10 5'  }} />
+                    <ChartsXAxis />
+                    <ChartsYAxis />
+                </ChartContainer>
+            </Box>
         </Container>
     );
 }
