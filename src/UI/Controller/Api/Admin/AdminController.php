944ab6ab13f2f555c2b126e03e076e5a352c1e37
Tomasz Kaliński <tomkalon@gmail.com>
2024-06-12 18:45:44 +0200

    refresh token bundle

--- src/UI/Controller/Api/Admin/AdminController.php
+++ src/UI/Controller/Api/Admin/AdminController.php
@@ -3,8 +3,12 @@
 namespace App\UI\Controller\Api\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class AdminController extends AbstractController
 {
-
+    public function test():Response
+    {
+        return $this->json(true);
+    }
 }
